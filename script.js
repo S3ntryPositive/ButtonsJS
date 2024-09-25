47bcdf6b748e2c707b77f13db2e7d9558db835d8
S3ntry_Positive <89636989+S3ntryPositive@users.noreply.github.com>
2024-09-25 10:13:30 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+window.addEventListener("load", addListener);
 
+function addListener()
+{
+	document.getElementById("inpResult").disabled = true;
+	document.getElementById("BtnAdd").addEventListener("click",Addition);
+	document.getElementById("BtnSub").addEventListener("click",Addition);
+	document.getElementById("BtnMul").addEventListener("click",Addition);
+	document.getElementById("BtnDiv").addEventListener("click",Addition);
+	document.getElementById("BtnClr").addEventListener("click",Addition);
+	
+}
